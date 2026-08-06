f7a31ae953ac45373ccc3dedf47c14704a8bb8dd
Alfonso Lara <orozcoenrique159@gmail.com>
2026-08-05 19:54:13 -0500

    clase producto agregada como bosquejo

---
+++ clase2/proyecto-final/models/producto.py
@@ -0,0 +1,34 @@
+class Producto:
+    def __init__(self):
+        """
+            Constructor de la clase Producto.
+
+            Esta método recibirá los atributos necesarios para representar un producto.
+        """
+
+
+    def mostrar(self):
+        """
+            Muestra la información del producto.
+        """
+        print("Método mostrar() pendiente de implementar")
+
+    def aumentar_stock(self):
+        """
+        Aumenta el stock disponible.
+        """
+        print("Método aumentar_stock() pendiente")
+
+    def disminuir_stock(self):
+        """
+            Disminuye el stock disponible
+        """
+        print("Mpetodo disminuir_stock() pendiente")
+
+    def hay_stock(self):
+        """
+            Verifica si existe stock suficiente
+        """
+        print("Método hay_stock() pendiente")
+    
+    
---
+++ clase2/proyecto-final/models/venta.py
@@ -0,0 +1,13 @@
+class Venta:
+    def __init__(self):
+        """
+            Constuctor de la clase Venta
+        """
+        pass
+
+
+    def calcular_total(self):
+        """
+            Método para calcular el total de la venta
+        """
+        print("Método calcular_total() pendiente")
