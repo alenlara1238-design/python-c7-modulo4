2868d1800c2ccae910a753822452f8d7d6c8d53e
Alfonso Lara <orozcoenrique159@gmail.com>
2026-08-05 20:08:55 -0500

    bosquejo de  clase inventario finalizado

---
+++ clase2/proyecto-final/services/inventario.py
@@ -0,0 +1,37 @@
+class Inventario:
+
+
+    def __init__(self):
+        """
+            Aqui represento el inventario en una estructra de lista, porque me interesa
+            e orden de inserción.
+        """
+        self.productos = []
+
+    def registrar_producto(self):
+        """
+            Permite agregar productos al inventario.
+        """
+        print("\n[Inventario]")
+        print("Registrar producto (pendiente).")
+
+    def listar_producto(self):
+
+        print("\n[Inventario]")
+        print("Listar productos (pendiente).")
+
+    def buscar_producto(self):
+        print("\n[Inentario]")
+        print("Buscar producto (pendiente).")
+
+    def eliminar_producto(self):
+        print("\n[Inentario]")
+        print("Eliminar producto (pendiente).")
+
+    def mostrar_cantidad_productos(self):
+        """
+            Este método se puede implementar si necesitamos conocer cuantos productos tenemos en inventario.
+        """
+        print("mostrar cantidad de productos (pendiente).")
+
+    
